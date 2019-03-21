85b1bf06f1e4ed0421a7e4383a6b7208d02db31e
helgrima <arttu.tamminen@kapsi.fi>
2019-03-21 21:55:19 +0200

    description of database and functors to show data about event and votes

--- eventshuffle.pl
+++ eventshuffle.pl
@@ -2,33 +2,50 @@
 start.
 
 :- dynamic occasion/2.
-:- dynamic happens/2.
+:- dynamic happens/3.
 :- dynamic participant/2.
 :- dynamic votes/3.
 
+% First term is unique identifier
+% Second term is name of occasion
 occasion(0, "Jake's secret party").
 occasion(1, "Bowling night").
 occasion(2, "Tabletop gaming").
 
-happens(0, "2014-01-01").
-happens(0, "2014-01-05").
-happens(0, "2014-01-12").
+% First term is unique identifier
+% Second term is reference to occasion
+% Third term is date
+happens(0, 0, "2014-01-01").
+happens(1, 0, "2014-01-05").
+happens(2, 0, "2014-01-12").
 
+% First term is unique identifier
+% Second term is name of participant
 participant(0, "John").
 participant(1, "Julia").
 participant(2, "Paul").
 participant(3, "Daisy").
 participant(4, "Dick").
 
+% First term is reference to participant
+% Second term is refetence to happens
 votes(0, 0).
 votes(1, 0).
 votes(2, 0).
 votes(3, 0).
+votes(4, 0).
 votes(4, 1).
 
 show_occasion(OccasionId, occasion(OccasionId, OccasionName, OccasionDate)):-
     occasion(OccasionId, OccasionName),
-    happens(OccasionId, OccasionDate).
+    happens(_, OccasionId, OccasionDate).
 
-list_occasion(occasion(OccasionId, OccasionName)):-
+list_occasions(occasion(OccasionId, OccasionName)):-
     occasion(OccasionId, OccasionName).
+
+participants_votes(ParticipantId, occasion_vote(participant(ParticipantName), occasion(OccasionId, OccasionName, OccasionDate))) :-
+    participant(ParticipantId, ParticipantName),
+    votes(ParticipantId, HappensId),
+    happens(HappensId, OccasionId, OccasionDate),
+    occasion(OccasionId, OccasionName).
+
